258fb98dd8a8e37fdfeb84ec0329324e9ec6d6bc
Miguel Fernandez <miguelrf95@gmail.com>
2019-10-09 17:27:03 +0100

    Add methods to get different vars at the same time

--- Tuples/Program.cs
+++ Tuples/Program.cs
@@ -4,9 +4,75 @@ namespace Tuples
 {
     class Program
     {
+        private static Random rnd;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            rnd = new Random();
+            Stuff s;
+            Tuple<int, double, bool> tp;
+            int i;
+            double d;
+            bool b;
+
+            Object[] objs = GetStuff1();
+
+
+            Console.WriteLine("First method to get stuff");
+            foreach (Object obj in objs)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("Second method to get stuff");
+            s = GetStuff2();
+            Console.WriteLine($"Int = {s.Intager}, " +
+                $"Double = {s.Double}, " +
+                $"Bool = {s.Bool}");
+
+            Console.WriteLine("Third method to get stuff");
+            GetStuff3(out i, out d, out b);
+            Console.WriteLine($"Int = {i}, " +
+                $"Double = {d}, " +
+                $"Bool = {b}");
+
+            Console.WriteLine("Fourth method to get stuff");
+            tp = GetStuff4();
+            Console.WriteLine($"Tuple's int:{tp.Item1}");
+            Console.WriteLine($"Tuple's int:{tp.Item2}");
+            Console.WriteLine($"Tuple's int:{tp.Item3}");
+        }
+
+        // Static Methods
+        private static Object[] GetStuff1()
+        {
+            Object[] objs = new Object[3];
+            objs[0] = rnd.Next();
+            objs[1] = rnd.NextDouble();
+            objs[2] = rnd.NextDouble() < 0.5f;
+
+            return objs;
+        }
+
+        private static Stuff GetStuff2()
+        {
+            return new Stuff(rnd.Next(), rnd.NextDouble(),
+                rnd.NextDouble() < 0.5f);
+        }
+
+        private static void GetStuff3(out int i, out double d, out bool b)
+        {
+            i = rnd.Next();
+            d = rnd.NextDouble();
+            b = rnd.NextDouble() < 0.5f;
+        }
+
+        private static Tuple<int, double, bool> GetStuff4()
+        {
+            Tuple<int, double, bool> t = new Tuple<int, double, bool>
+                (rnd.Next(), rnd.NextDouble(), rnd.NextDouble() < 0.5f);
+
+            return t;
         }
     }
 }
---
+++ Tuples/Stuff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuples
+{
+    struct Stuff
+    {
+        //Struct properties
+        public int Intager { get; }
+        public double Double { get; }
+        public bool Bool { get; }
+
+        // Constructor
+        public Stuff(int _intager, double _double, bool _bool)
+        {
+            Intager = _intager;
+            Double = _double;
+            Bool = _bool;
+        }
+    }
+}
